4285be0d32b5ef446ca3feefed82babad88c7947
jordan <jordan.los@hotmail.com>
2015-09-04 14:23:22 -0600

    Change First example cardset on hover

--- site/_build/assets/js/script.js
+++ site/_build/assets/js/script.js
@@ -1,3 +1,48 @@
-$( window ).load(function() {
-	$( 'img' ).resizeRhythm(12);
+$( document ).ready(function() {
+	/*
+	$('.click-me').click(function() {
+		//Highlights clicked tab
+		$( this ).addClass( "focus" );
+		//Unhighlights other tabs
+		$( '.click-me' ).not( this ).removeClass( 'focus' );
+		//Finds the clicked tab's index number
+		var self  = $( this ).index();
+		//Matches the tab to its card
+		var	matchedTarget = $( '.click-me-object' ).eq(self);
+		//Displays the card
+		matchedTarget.css("display", "block" );
+		//Hides all other cards
+		$( '.click-me-object' ).not(matchedTarget).css("display", "none");
+	});
+	*/
+	function switchCard() {
+		//Highlights clicked tab
+		$( this ).addClass( "focus" );
+		//Unhighlights other tabs
+		$( '.click-tab-place' ).not( this ).removeClass( 'focus' );
+		//Finds the clicked tab's index number
+		var self  = $( this ).index();
+		//Matches the tab to its card
+		var	matchedTarget = $( '.click-me-object' ).eq(self);
+		//Displays the card
+		matchedTarget.css("display", "block" );
+		//Hides all other cards
+		$( '.click-me-object' ).not(matchedTarget).css("display", "none");
+	};
+	
+	$( '.click-tab-place' ).hover(switchCard);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
